838e981013bd7d06d12881e31896c7a1318797d9
GRBrian-tech <garridorellanb8@gmail.com>
2026-05-08 20:14:36 +0200

    Add files via upload

---
+++ Informatica.py
@@ -0,0 +1,26 @@
+print("Hello i am schlue bot ")
+name=input("what is your name? ")
+antwort=input("how was your day today? ")
+if antwort=="good":
+    print("goood!that makes me happy! ")
+elif antwort=="bad":
+    print("Come head up!")
+elif antwort=="ok":
+    print("ok have a good day!")
+else:print("sorry i do not understand but y hope you are fine!")
+
+antwort=input("what did you have at achool today mathematics,german or geography?")
+if antwort=="mathematics":
+    print("cool y love mathematics!")
+elif antwort=="german":
+    print("Ja das ist den toll!")
+elif antwort=="geography":
+    print("Wow which countrys do you have learned today? ")
+else: print("sorry but y can not understand you")
+
+antwort=input("which colour do you prefer green or red? ")
+if antwort=="green":
+    print("wonderfull y love green to!")
+elif antwort=="red":
+    print("Wow y love red also!")
+print("goobye " + name + "! It was nice talking to you.")
