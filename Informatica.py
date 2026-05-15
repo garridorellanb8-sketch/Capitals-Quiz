8fb066b9e8b4d50697bf63d4d2e114e36d39c481
GRBrian-tech <garridorellanb8@gmail.com>
2026-05-15 23:31:13 +0200

    Refactor dialogue and correct spelling errors

--- Informatica.py
+++ Informatica.py
@@ -7,20 +7,20 @@ elif antwort=="bad":
     print("Come head up!")
 elif antwort=="ok":
     print("ok have a good day!")
-else:print("sorry i do not understand but y hope you are fine!")
+else:print("sorry I do not understand but I hope you are fine!")
 
-antwort=input("what did you have at achool today mathematics,german or geography?")
+antwort=input("what did you have at school today mathematics,german or geography?")
 if antwort=="mathematics":
-    print("cool y love mathematics!")
+    print("cool I love mathematics!")
 elif antwort=="german":
     print("Ja das ist den toll!")
 elif antwort=="geography":
-    print("Wow which countrys do you have learned today? ")
-else: print("sorry but y can not understand you")
+    print("Wow which countries do you have learned today? ")
+else: print("sorry but I can not understand you")
 
 antwort=input("which colour do you prefer green or red? ")
 if antwort=="green":
-    print("wonderfull y love green to!")
+    print("wonderfull I love green to!")
 elif antwort=="red":
-    print("Wow y love red also!")
+    print("Wow I love red also!")
 print("goobye " + name + "! It was nice talking to you.")
